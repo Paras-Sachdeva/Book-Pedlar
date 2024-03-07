820ee21d5c32c672049850a121d1b49bc6e1de9e
Paras-Sachdeva <paras140902@gmail.com>
2024-03-07 22:58:23 +0530

    STYLE: Book Info Page

--- FullBookInfo.php
+++ FullBookInfo.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Book Pedlar - Book Info</title>
     <link rel="icon" href="Images/Icon.png" type="image/x-icon">
-    <link rel="stylesheet" href="styles.css?v=5">
+    <link rel="stylesheet" href="Styles/styles.css?v=5">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-<body style='overflow-x:hidden;'>
-<?php
+<body>
+    <?php
             require("./Components/loader.php");  //Loader Component
 
             echo('<div class="content">');
@@ -110,7 +110,7 @@
             mysqli_close($conn);
             require("./Components/footer.php");  
             echo("</div></div>");
-?> 
+    ?> 
         <script>
             let jsBookId=<?php echo json_encode($selected_bookid); ?>;
             let jsBookPhoto=<?php echo json_encode($photo7); ?>;
--- book_process.php
+++ book_process.php
@@ -16,32 +16,31 @@
             
             if (!$conn) {
                 die("Connection failed");}
+  
+            $fileName = $_FILES["bookPicture"]["name"];
+            $fileTmpName = $_FILES["bookPicture"]["tmp_name"];
+            $fileSize = $_FILES["bookPicture"]["size"];
+            $fileType = $_FILES["bookPicture"]["type"];
 
-                
-                    $fileName = $_FILES["bookPicture"]["name"];
-                    $fileTmpName = $_FILES["bookPicture"]["tmp_name"];
-                    $fileSize = $_FILES["bookPicture"]["size"];
-                    $fileType = $_FILES["bookPicture"]["type"];
+            $destination = "uploads/" . $fileName;
+            move_uploaded_file($fileTmpName, $destination);
 
-                    $destination = "uploads/" . $fileName;
-                    move_uploaded_file($fileTmpName, $destination);
-
-                    $book_name=$_REQUEST["bookName"];
-                    $capitalize_book_name=ucwords($book_name);
-                    $author_name=$_REQUEST["authorName"];
-                    $capitalize_author_name=ucwords($author_name);
-                    $publisher=$_REQUEST["publisher"];
-                    $capitalize_publisher=ucwords($publisher);
-                    $mrp=$_REQUEST["mrp"];
-                    $selling_price=$_REQUEST["sellingPrice"];
-                    $genre=$_REQUEST["genre"];
-                    $condition=$_REQUEST["condition"];
-                    $additional_info=$_REQUEST["additionalInfo"];
+            $book_name=$_REQUEST["bookName"];
+            $capitalize_book_name=ucwords($book_name);
+            $author_name=$_REQUEST["authorName"];
+            $capitalize_author_name=ucwords($author_name);
+            $publisher=$_REQUEST["publisher"];
+            $capitalize_publisher=ucwords($publisher);
+            $mrp=$_REQUEST["mrp"];
+            $selling_price=$_REQUEST["sellingPrice"];
+            $genre=$_REQUEST["genre"];
+            $condition=$_REQUEST["condition"];
+            $additional_info=$_REQUEST["additionalInfo"];
         
-                    $sql="INSERT INTO book_data (bookname,userid,author,publisher,actualprice,sellprice,bookstatus,genre,bookcondition,addinfo,photo) VALUES ('$capitalize_book_name','$userid','$capitalize_author_name','$capitalize_publisher',$mrp,$selling_price,'Available','$genre','$condition','$additional_info','$fileName')";
+            $sql="INSERT INTO book_data (bookname,userid,author,publisher,actualprice,sellprice,bookstatus,genre,bookcondition,addinfo,photo) VALUES ('$capitalize_book_name','$userid','$capitalize_author_name','$capitalize_publisher',$mrp,$selling_price,'Available','$genre','$condition','$additional_info','$fileName')";
 
-                    mysqli_query($conn,$sql);
-                    header("Location: Dashboard.php");
+            mysqli_query($conn,$sql);
+            header("Location: Dashboard.php");
     ?>
 </body>
 </html>
