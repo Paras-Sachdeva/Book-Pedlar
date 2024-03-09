712391b625bc1106331d17f6979cc0a93e1d7b7c
Paras-Sachdeva <paras140902@gmail.com>
2024-03-09 23:13:42 +0530

    FIX: Display Notifications Only When Available

--- dashboard.php
+++ dashboard.php
@@ -66,39 +66,40 @@
             <div class="add-book">
                 <button id="openPageButton">ADD BOOK</button>
             </div>
-            <div class="outside-notifications">
-                <div class="notifications"><H2>NOTIFICATIONS</H2></div>
-                <div class="new-notification">
-                    <?php
-                        $arr4=array();
-                        $arr5=array();
-                        $i=0;
-                        $sql2 = "SELECT * FROM user_notification WHERE userid='$userid'";
-                        $result2 = mysqli_query($conn,$sql2);
-                        if(mysqli_num_rows($result2)>0){
-                            while($row2=mysqli_fetch_assoc($result2)){
-                                $senderId=$row2['senderid'];
-                                $sql3="SELECT * FROM user_data WHERE id='$senderId'";
-                                $result3=mysqli_query($conn,$sql3);
-                                $row3=mysqli_fetch_assoc($result3);
-                                array_push($arr4,$row3['profileImage']);
-                                array_push($arr5,$senderId);
-                                $bookId=$row2['bookid'];
-                                $sql5="SELECT * FROM book_data WHERE id='$bookId'";
-                                $result5=mysqli_query($conn,$sql5);
-                                $row5=mysqli_fetch_assoc($result5);
-                                $bookName=$row5['bookname'];
-                                echo("  <div class='notify' id='$i'>
-                                            <div class='small-buyer-pic' id='$senderId-$i'>
-                                            </div>
-                                            <div class='notification-content'>$bookName</div> 
-                                        </div>");
-                                $i++;
-                            }
-                        }
-                    ?>
-                </div>
-            </div>
+            <?php
+                $sql2 = "SELECT * FROM user_notification WHERE userid='$userid'";
+                $result2 = mysqli_query($conn,$sql2);
+                $arr4=array();
+                $arr5=array();
+                if(mysqli_num_rows($result2)>0){            
+                    echo("<div class='outside-notifications'>
+                                <div class='notifications'>
+                                    <H2>NOTIFICATIONS</H2>
+                                </div>
+                                <div class='new-notification'>");
+                    $i=0;
+                    while($row2=mysqli_fetch_assoc($result2)){
+                        $senderId=$row2['senderid'];
+                        $sql3="SELECT * FROM user_data WHERE id='$senderId'";
+                        $result3=mysqli_query($conn,$sql3);
+                        $row3=mysqli_fetch_assoc($result3);
+                        array_push($arr4,$row3['profileImage']);
+                        array_push($arr5,$senderId);
+                        $bookId=$row2['bookid'];
+                        $sql5="SELECT * FROM book_data WHERE id='$bookId'";
+                        $result5=mysqli_query($conn,$sql5);
+                        $row5=mysqli_fetch_assoc($result5);
+                        $bookName=$row5['bookname'];
+                        echo("  <div class='notify' id='$i'>
+                                    <div class='small-buyer-pic' id='$senderId-$i'>
+                                    </div>
+                                    <div class='notification-content'>$bookName</div> 
+                                </div>");
+                        $i++;
+                    }
+                    echo("</div></div>");
+                }
+            ?>
         </div>
         <!-- User Uploaded Book Section -->
         <div class="user-books">
@@ -265,6 +266,7 @@
             document.addEventListener("DOMContentLoaded", function() {
                 var openPageButton = document.getElementById("openPageButton");
                 openPageButton.addEventListener("click", function() {
+                    console.log("hey ho gya click");
                     window.location.href = "addBook.php";
                 });
             });
@@ -298,16 +300,16 @@
             });
 
             // Upload Notification Pics
-            let jsSmallPhoto=<?php echo json_encode($arr4); ?>;
-            console.log(jsSmallPhoto);
-            let jsSmallSenderId=<?php echo json_encode($arr5); ?>;
-            for(let i=0;i<jsSmallPhoto.length;i++){
-                let jsSmallPhotoTag=document.getElementById(jsSmallSenderId[i]+'-'+i);
-                console.log(jsSmallSenderId[i]+'-'+i);
-                console.log(jsSmallPhotoTag);
-                jsSmallPhotoTag.style.backgroundImage="url('Uploads/"+jsSmallPhoto[i]+"')";
-                jsSmallPhotoTag.style.backgroundSize="60px 60px";    
-            }
+                let jsSmallPhoto=<?php echo json_encode($arr4); ?>;
+                console.log(jsSmallPhoto);
+                let jsSmallSenderId=<?php echo json_encode($arr5); ?>;
+                for(let i=0;i<jsSmallPhoto.length;i++){
+                    let jsSmallPhotoTag=document.getElementById(jsSmallSenderId[i]+'-'+i);
+                    console.log(jsSmallSenderId[i]+'-'+i);
+                    console.log(jsSmallPhotoTag);
+                    jsSmallPhotoTag.style.backgroundImage="url('Uploads/"+jsSmallPhoto[i]+"')";
+                    jsSmallPhotoTag.style.backgroundSize="60px 60px";    
+                }
     </script>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
