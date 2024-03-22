c5b274593bbf6f9d769aec7653d8543fab90ec7a
Paras-Sachdeva <paras140902@gmail.com>
2024-03-22 23:55:36 +0530

    ADD: Sample Text to Footer

--- Components/footer.php
+++ Components/footer.php
@@ -1,1 +1,11 @@
-<div class="footer"></div>
+<div class="footer">
+   <div class="footer-content" style="text-align:center;padding-top:1rem">
+       <p style="color:white;">&copy; 2024 Your Company Name. All rights reserved.</p>
+       <br><br>
+       <ul class="footer-links">
+         <li><a href="#">Privacy Policy</a></li>
+         <li><a href="#">Terms of Service</a></li>
+         <li><a href="#">Contact Us</a></li>
+       </ul>
+    </div>
+</div>
