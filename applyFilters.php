64b1798179dbeb5b82fd7e284f909d9994612bb4
Paras-Sachdeva <paras140902@gmail.com>
2024-04-08 22:43:17 +0530

    STYLE: Notify Me Confirmation Box

--- applyFilters.php
+++ applyFilters.php
@@ -669,5 +669,6 @@
             window.location.href="loginPage.php";
         });
     </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </body> 
 </HTML>
